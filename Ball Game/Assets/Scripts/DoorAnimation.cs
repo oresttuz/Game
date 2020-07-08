9608ffb1cddcfe6367c84a7913ee297141c37060
Orest Tuz <orest.tuz.lex@gmail.com>
2020-07-08 17:31:27 -0400

    Doors Work

---
+++ Ball Game/Assets/Scripts/DoorAnimation.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorAnimation : MonoBehaviour
+{
+    public bool PlayerOpened;
+
+    public GameObject Left, Right;
+
+    private void FixedUpdate()
+    {
+        if (PlayerOpened)
+        {
+            Left.GetComponent<Animator>().SetBool("Open", true);
+            Right.GetComponent<Animator>().SetBool("Open", true);
+        }
+    }
+}
---
+++ Ball Game/Assets/Scripts/DoorDetect.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorDetect : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.name == "Player")
+        {
+            this.GetComponentInParent<DoorAnimation>().PlayerOpened = true;
+        }
+    }
+}
